dae501a7f6322b0b54f332a490b6a99300f3b8d5
bblackskulll063 <98683404+bblackskulll063@users.noreply.github.com>
2023-07-29 16:55:07 +0530

    console change

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 require("dotenv").config();
-
+console.log(process.env.key_password);
 const db = process.env.key_password;
 mongoose.connect(db)
   .then(() =>{
